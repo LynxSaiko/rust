7f600f6370c76e4b029377b88322e6357e160805
LynxSaiko <redteamsectorid@gmail.com>
2025-07-27 06:00:20 +0700

    Create ix.sh

---
+++ ix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export LIBSSH2_SYS_USE_PKG_CONFIG=1 &&
+DESTDIR=${PWD}/install python3 ./x.py install &&
+unset LIBSSH2_SYS_USE_PKG_CONFIG
+
+chown -R root:root install &&
+cp -a install/* /
